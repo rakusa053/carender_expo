72e313a0ddf8693c9cd2e37fe314751094cd1ab3
sakuramochi053 <sakuramochi053@gmail.com>
2025-12-05 06:58:32 +0900

    入力欄の追加

--- app/(tabs)/about/[id].tsx
+++ app/(tabs)/about/[id].tsx
@@ -1,22 +1,34 @@
-import { Stack, useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from 'react-native';
-
+import { TextInput } from "react-native-gesture-handler";
 
 
 
 export default function dayscrren() {
   const { id ,month} = useLocalSearchParams();
+  const [name, setName] = useState<string>("");
   return (
     <>
     <View style={styles.days}>
-       <Text style={[styles.text,{marginTop:150}]}> {month}月{id}日</Text>
+       <Text style={[styles.monthtext,{marginTop:150},]}> {month}月{id}日</Text>
       
+    
+<TextInput 
+style ={styles.inputtext}
+value = {name}
+onChangeText={setName}
+placeholder="金額を入力してください"
+></TextInput>
+
     </View>
-      <Stack.Screen 
+      {/* <Stack.Screen 
         options={{
           headerTitle: `2025年: ${id}日`, 
         }} 
-      />
+      /> */}
+
+
 
       <View style={styles.container}>
         <Text style={styles.text}> {id}日です</Text>
@@ -31,7 +43,7 @@ const styles = StyleSheet.create({
     flex:1,
     backgroundColor: '#ffffffff',
     justifyContent:'flex-start',
-    alignItems:'center'
+    alignItems:'center',
 
   },
   container: {
@@ -40,7 +52,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
+  monthtext: {
     color: '#000000ff',
+    fontSize:50,
+  },
+    inputtext: {
+    color: '#000000ff',
+    fontSize:20,
+  },
+    text: {
+    color: '#000000ff',
+    fontSize:10,
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -13,6 +14,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -21,5 +23,6 @@ export default function RootLayout() {
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
